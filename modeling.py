786342e097c31e15cd9895677d8db466f7926666
Haozhe-py <xuhaozhepy@outlook.com>
2025-07-13 18:30:51 +0800

    Add files via upload

--- modeling.py
+++ modeling.py
@@ -1,8 +1,22 @@
 from torch import nn
 from torch.nn import functional as F
+from torch import distributed as dist
 from dataclasses import dataclass
 import torch
 
 @dataclass
 class ModelArgs:
-    pass
+    # embedding
+    dim:int = 7168
+    world_size:int = 1
+    fine_tune_from_deepseek:bool = True
+
+class ParallelEmbedding(nn.Module):
+    # Embedding layer with parallelism support across distributed processes.
+
+    # Args:
+    #     vocab_size (int) : vocabulary size.
+    #     dim (int) : Embedding dimension.
+
+    def __init__(self, vocab_size:int, dim:int) -> None :
+        super().__init__()
