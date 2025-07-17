6e87028a4ada9b4a03bbb05f1e3c5b3ca2b30e9a
Haozhe <xuhaozhepy@outlook.com>
2025-07-17 12:55:50 +0800

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -1,13 +1,19 @@
+# Eternity-V1 modeling.py
+# Edited by Haozhe Xu, Eternity-LLM Organization
+
 from torch import nn
 from torch.nn import functional as F
 from torch import distributed as dist
 from dataclasses import dataclass
 from typing import Tuple, Optional, Literal
+from .kernel import act_quant, weight_dequant, fp8_gemm
 import torch
 
 world_size = 1
 rank = 0
 use_deepseek = True
+block_size = 128
+gemm_impl = 'bf16'
 
 @dataclass
 class ModelArgs:
@@ -56,3 +62,34 @@ class ParallelEmbedding(nn.Module):
             y[mask] = 0
             dist.all_reduce(y)
         return y
+
+class Linear(nn.Module):
+    #
+    defalt_dtype = torch.bfloat16
+    
+    def __init__(self, in_features:int, out_features:int, bias:bool = False, dtype = None) -> None:
+        super().__init__()
+        self.weight = nn.Parameter(torch.empty(in_features, out_features, dtype = dtype or Linear.defalt_dtype))
+        self.__bias = bias
+        if self.weight.element_size() == 1:
+            scale_out_features = (out_features + block_size - 1) // block_size
+            scale_in_features = (in_features + block_size - 1) // block_size
+            self.weight.scale = self.scale = nn.Parameter(torch.empty(scale_in_features, scale_out_features, dtype=torch.float32))
+        else:
+            self.register_parameter("scale", None)
+        if bias:
+            self.bias = nn.Parameter(torch.empty(out_features))
+        else:
+            self.register_parameter("bias", None)
+            
+	def forward(self, x:torch.Tensor):
+        if self.weight.element_size() > 1:
+        	weighted_sum = torch.matmul(x, self.weight)
+        elif gemm_impl == 'bf16':
+            weighted_sum = torch.matmul(x, weight_dequant(self.weight, self.weight.scale))
+        else:
+            x, scale = act_quant(x, block_size)
+        	weighted_sum = fp8_gemm(x, scale, weight, weight.scale)
+        if self.__bias:
+            weighted_sum += self.bias
+        return weighted_sum
