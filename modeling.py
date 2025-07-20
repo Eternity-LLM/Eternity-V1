b37eae3ef30bf929a72bba293b98de2ad71dc6fc
Haozhe <xuhaozhepy@outlook.com>
2025-07-20 12:58:48 +0800

    Update

--- modeling.py
+++ modeling.py
@@ -10,6 +10,7 @@ from dataclasses import dataclass
 from typing import Tuple, Optional, Literal
 from .kernel import act_quant, weight_dequant, fp8_gemm
 import torch
+import math
 from . import utils as u
 
 
@@ -22,7 +23,7 @@ gemm_impl = 'bf16'
 @dataclass
 class ModelArgs:
     max_batch_size:int = 16
-    max_seq_len:int = 524288
+    max_seq_len:int = 163840 #524288
     dtype:Literal['fp8', 'bf16'] = 'bf16'
     # embedding
     dim:int = 7168
@@ -51,6 +52,7 @@ class ModelArgs:
     beta_slow:int = 1
     rope_theta:float = 10000.0
     rope_factor:float = 40.0
+    original_seq_len:int = 4096
 
 
 class ParallelEmbedding(nn.Module):
@@ -167,4 +169,31 @@ def precompute_freqs_cis(args:ModelArgs) -> torch.Tensor :
     base = args.rope_theta
     factor = args.rope_factor
 
+    def find_correction_dim(n_rotations, dim, base, max_seq_len):
+        return dim * math.log(max_seq_len / (n_rotations * 2 * math.pi)) / (2 * math.log(base))
+    def find_correction_range(low_rot, high_rot, dim, base, max_seq_len):
+        low = math.floor(find_correction_dim(low_rot, dim, base, max_seq_len))
+        high = math.ceil(find_correction_dim(high_rot, dim, base, max_seq_len))
+        return max(low, 0), min(high, dim - 1)
+    def linear_ramp_factor(min_val, max_val, dim):
+        if min_val == max_val:
+            max += 0.001
+        linear_func = (torch.arange(dim, dtype = torch.float32) - min_val) / (max_val - min_val)
+        ramp_func = torch.clamp(linear_func, 0, 1)
+        return ramp_func
+    
+    freqs = 1.0 / (base ** (torch.arange(0, dim, 2, dtype = torch.float32) / dim))
+    if seqlen > args.original_seq_len:
+        low, high = find_correction_range(bf, bs, dim, base, args.original_seq_len)
+        smooth = 1 - linear_ramp_factor(low, high, dim // 2)
+        freqs = freqs / factor * (1 - smooth) + freqs * smooth
+
+	t = torch.arange(seqlen)
+    freqs = torch.outer(t, freqs)
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
+    return freqs_cis
+
+def apply_rope(x:torch.Tensor, freqs_cis:torch.Tensor) -> torch.Tensor :
+    dtype = x.dtype
+    x = torch.view_as_complex(x.float().view(*x.shape[:-1], -1, 2))
     
