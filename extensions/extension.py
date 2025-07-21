2479a679e07c8fe21b9952c8809c2373e52c4fe5
Haozhe <xuhaozhepy@outlook.com>
2025-07-21 12:42:50 +0800

    Create extension.py

---
+++ extensions/extension.py
@@ -0,0 +1,12 @@
+
+class Extension:
+    begin_label = None
+    end_label = None
+    system_prompt = None
+    output_prompt = None
+    def __init__(self):
+        pass
+    def forward(self, input_str):
+        pass
+    def __call__(self, input_str):
+        pass
