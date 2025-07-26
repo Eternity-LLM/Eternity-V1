af7583d30570933cffdb7bb8abbb58d687885683
Haozhe <xuhaozhepy@outlook.com>
2025-07-26 12:42:58 +0800

    Update via github.dev/

--- utils.py
+++ utils.py
@@ -1,3 +1,31 @@
 import torch
 import torch.nn.functional as F
 
+def f(x):
+	idx_1 = x>=0.
+    idx_2 = x<0.
+    x[idx_1] = (x[idx_1] + 1.) ** 2
+    x[idx_2] = 1. / ((x[idx_2] - 1.) ** 2 )
+    return x
+
+def f_sigmoid(x, s:float = 3.0):
+    # "sigmoid" function with function f instead of exp
+    return 1./(1.+f(-x*s))
+
+def f_silu(x, s:float = 3.0):
+    # "silu" function with function f instead of exp
+    return x*f_sigmoid(x, s)
+
+def f_softmax(x, s:float = 3.0, dim:int = -1):
+    # "softmax" function with function f instead of exp
+    x = f(x*s)
+    s = torch.sum(x, dim = dim, keepdim = True)
+    return x / s
+
+def m(x):
+    idx_1 = x>=0.
+    idx_2 = x<0.
+    x[idx_1] = x[idx_1] + 1.
+    x[idx_2] = 1. / (1. - x[idx_2])
+    return x
+
