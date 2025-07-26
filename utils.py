11224f867bafe800d0178db84cd422c218669eb1
Haozhe <xuhaozhepy@outlook.com>
2025-07-26 18:16:42 +0800

    Update

--- utils.py
+++ utils.py
@@ -29,3 +29,21 @@ def m(x):
     x[idx_2] = 1. / (1. - x[idx_2])
     return x
 
+def g(x):
+    idx_1 = x<1.
+    idx_2 = x>=1.
+    x[idx_1] = 1.0-torch.rsqrt(x)
+    x[idx_2] = torch.sqrt(x)-1.0
+    return x
+
+def loss(y, y_pred):
+    bsz, seq_len, dim = y.shape
+    y = y.reshape(bsz * seq_len, dim)
+    y_pred = y_pred.reshape(bsz * seq_len, dim)
+    loss = torch.zeros_like(y)
+    for i in range(dim):
+        loss[:, i] = - y[:, i] * g(y_pred[:, i]) - (1. - y[:, i]) * g(1. - y_pred[:, i])
+    loss = loss.reshape(bsz, seq_len, dim)
+    loss = torch.sum(loss, dim=-1)
+    return loss
+
