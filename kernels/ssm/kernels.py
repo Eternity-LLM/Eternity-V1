eb252816db3a75284a3100cb764a6d301f6722f1
Haozhe <xuhaozhepy@outlook.com>
2025-10-04 12:58:08 +0800

    Update

--- kernels/ssm/kernels.py
+++ kernels/ssm/kernels.py
@@ -16,7 +16,21 @@ import triton.language as tl
 
 from einops import rearrange, repeat
 
-from mamba_ssm.ops.triton.softplus import softplus
+from packaging import version
+
+TRITON3 = version.parse(triton.__version__) >= version.parse("3.0.0")
+
+
+if TRITON3:
+    @triton.jit
+    def softplus(dt):
+        return tl.math.log(tl.math.exp(dt) + 1)
+else:
+    @triton.jit
+    def softplus(dt):
+        return tl.math.log1p(tl.exp(dt))
+
+# from mamba_ssm.ops.triton.softplus import softplus
 
 
 def init_to_zero(names):
